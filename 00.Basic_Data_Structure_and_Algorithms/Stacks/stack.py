88a972cc62f010334adfe3631e42a6b6b9898d55
mr.beaver <beavercoding@gmail.com>
2018-06-18 01:01:13 -0700

    started "stack" related implementation;

---
+++ 00.Basic_Data_Structure_and_Algorithms/Stacks/stack.py
@@ -0,0 +1,23 @@
+class Stack:
+	def __init__(self, newList = []):
+		self.stack = newList
+
+	def pop(self):
+		if len(self.stack):
+			return self.stack.pop()
+
+	def push(self, newVal = 0):
+		self.stack.append(newVal)
+		return self.stack
+
+	def top(self):
+		if len(self.stack):
+			return self.stack[-1]
+		else:
+			return None
+
+	def isEmpty(self):
+		return not len(self.stack)
+
+	def __str__(self):
+		return str(self.stack)
