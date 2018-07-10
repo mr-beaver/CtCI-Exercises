f28a579ca0af110c43cb8b1451efd7786ac573ad
mr.beaver <beavercoding@gmail.com>
2018-07-10 02:05:31 -0700

    added bitsetAnd and bitsetOr operations to bitset;

--- 00.Basic_Data_Structure_and_Algorithms/Bags_and_Sets/bit_set.py
+++ 00.Basic_Data_Structure_and_Algorithms/Bags_and_Sets/bit_set.py
@@ -19,6 +19,12 @@ class BitSet:
 	def bitsetClear(self, num):
 		self.store[self.bitSetIndex(num)] &= self.bitSetMask(num) ^ 63
 
+	def bitsetAnd(self, rightSet):
+		self.store = [(x & rightSet.store[i]) for i, x in enumerate(self.store)]
+
+	def bitsetOr(self, rightSet):
+		self.store = [(x | rightSet.store[i]) for i, x in enumerate(self.store)]
+
 	def __str__(self):
 		return str(["{0:b}".format(x) for x in self.store])
 
@@ -34,7 +40,16 @@ if __name__ == "__main__":
 	bs = BitSet()
 	bs.bitsetSet(1)
 	bs.bitsetSet(2)
-	print(bs)
-	print(bs.bitsetGet(0))
-	bs.bitsetClear(1)
-	print(bs)
+	print("bs1..{}".format(bs))
+	# print(bs.bitsetGet(0))
+	# bs.bitsetClear(1)
+	# print(bs)
+	bs2 = BitSet()
+	bs2.bitsetSet(2)
+	bs2.bitsetSet(3)
+	bs2.bitsetSet(64)
+	print("bs2..{}".format(bs2))
+	bs.bitsetAnd(bs2)
+	print("After bit set and ... {}".format(bs))
+	bs.bitsetOr(bs2)
+	print("After bit set or ... {}".format(bs))
